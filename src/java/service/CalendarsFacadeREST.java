30c8f5ac1b2e2672a5d1278fb431ca2c98826686
billescas <mrbim18@gmail.com>
2013-05-03 17:18:26 +0000

    Get de calendars

--- src/java/service/CalendarsFacadeREST.java
+++ src/java/service/CalendarsFacadeREST.java
@@ -5,11 +5,14 @@
 package service;
 
 import SOSCalendar.Calendars;
+import SOSCalendar.Dates;
 import SOSCalendar.Users;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -18,13 +21,14 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
  * @author borja
  */
 @Stateless
-@Path("soscalendar.calendars")
+@Path("")
 public class CalendarsFacadeREST extends AbstractFacade<Calendars> {
     @PersistenceContext(unitName = "SOSCalendarPU")
     private EntityManager em;
@@ -54,17 +58,25 @@ public class CalendarsFacadeREST extends AbstractFacade<Calendars> {
     }
 
     @GET
-    @Path("{id}")
+    @Path("{id_usu}/calendars/")
     @Produces({"application/xml", "application/json"})
     public Calendars find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
-    @GET
-    @Override
+    @GET    
+    @Path("{id_usu}/calendars")
     @Produces({"application/xml", "application/json"})
-    public List<Calendars> findAll() {
-        return super.findAll();
+    public List<Calendars> findAll(@PathParam("id_usu") Integer id_usu) {
+        //Primero, comprobamos que el usuario exista
+        checkUser(id_usu);
+        
+        String querytxt = "SELECT calendar_id FROM calendars c WHERE c.user_id = " + id_usu;
+        Query query = em.createQuery(querytxt);
+        List <Calendars> calendars = query.getResultList();
+        return calendars;
+        
+        
     }
 
     @GET
@@ -86,4 +98,19 @@ public class CalendarsFacadeREST extends AbstractFacade<Calendars> {
         return em;
     }
     
+        private void checkUser(int id) {
+        try {
+            em.createQuery("SELECT e FROM Users e where e.user_id = :user_id").setParameter("user_id", id).getSingleResult();
+        } catch (NoResultException ex) {
+            throw new WebApplicationException(new Throwable("User not found"), 404);
+	}
+    
+    }
+    
+    private Calendars checkCalendar(int id){
+	Calendars cal = super.find(id);
+	if(cal == null)
+	    throw new WebApplicationException(new Throwable("Calendar not found"), 404);
+	return cal;
+    }
 }
